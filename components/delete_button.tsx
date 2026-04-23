f0d54c7295c46e525f1eef6d61ffee5be4aaa8e0
Sepo14 <sepojra@gmail.com>
2026-04-23 11:56:40 +0200

    delete button fixed

--- components/delete_button.tsx
+++ components/delete_button.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 
 export default function DeleteButton({idTask}:{idTask: number}) {
     return (
-        <button type="submit" onClick={() => DeleteTask(idTask)} className="flex absolute bottom-5 right-40 gap-2 bg-gray-400 px-6 py-2 rounded-full cursor-pointer text-white">
+        <button type="button" onClick={() => DeleteTask(idTask)} className="flex right-40 gap-2 bg-gray-400 px-6 py-2 rounded-full cursor-pointer text-white">
             Delete
             <Image src={'Trash.svg'} alt='submit' width={25} height={25} />
         </button>
--- components/edit_form.tsx
+++ components/edit_form.tsx
@@ -23,9 +23,9 @@ type EditTaskFormProps = {
 export default function EditTaskForm({ closeForm, task }: EditTaskFormProps) {
 
     async function handleSubmit(formData: FormData) {
-            await UpdateTask(formData);
-            closeForm();
-        }
+        await UpdateTask(formData);
+        closeForm();
+    }
 
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("")
@@ -54,72 +54,74 @@ export default function EditTaskForm({ closeForm, task }: EditTaskFormProps) {
 
     return (
         <>
-        <Form action={handleSubmit} className="flex flex-col h-full justify-between overflow-auto">
-            <div className="flex flex-col items-start gap-1">
-                <label htmlFor="title" className="font-medium">Task name</label>
-                <input type="text" id="title" name="title" placeholder="Task name" value={title} onChange={(event) => setTitle(event.target.value)} className="border rounded-sm p-2 w-full" />
-                <label htmlFor="description">Description</label>
-                <textarea name="description" id="description" placeholder="Description" value={description} onChange={(event) => setDescription(event.target.value)} className="border rounded-sm p-2 h-40 w-full"></textarea>
-                <input type="hidden" name="idTask" value={id} />
-                <p>Icon</p>
-                <div className="flex gap-2">
-                    <label className="cursor-pointer p-3 bg-zinc-400 rounded-xl has-checked:bg-(--color-1) hover:bg-(--color-1)">
-                        <input type="radio" name="icon" id="icon1" value={'/joystick.svg'} checked={icon === "/joystick.svg"} onChange={(event) => setIcon(event.target.value)} className="hidden" />
-                        <Image src={'/joystick.svg'} alt='Console' width={'25'} height={'25'} />
-                    </label>
-                    <label className="cursor-pointer p-3 bg-zinc-400 rounded-xl has-checked:bg-(--color-1) hover:bg-(--color-1)">
-                        <input type="radio" name="icon" id="icon2" value={'online-learning.svg'} checked={icon === "online-learning.svg"} onChange={(event) => setIcon(event.target.value)} className="hidden" />
-                        <Image src={'online-learning.svg'} alt='Study' width={'25'} height={'25'} />
-                    </label>
-                    <label className="cursor-pointer p-3 bg-zinc-400 rounded-xl has-checked:bg-(--color-1) hover:bg-(--color-1)">
-                        <input type="radio" name="icon" id="icon3" value={'pingpong.svg'} checked={icon === "pingpong.svg"} onChange={(event) => setIcon(event.target.value)}  className="hidden" />
-                        <Image src={'pingpong.svg'} alt='Play' width={'25'} height={'25'} />
-                    </label>
-                    <label className="cursor-pointer p-3 bg-zinc-400 rounded-xl has-checked:bg-(--color-1) hover:bg-(--color-1)">
-                        <input type="radio" name="icon" id="icon4" value={'weightlifting.svg'} checked={icon === "weightlifting.svg"} onChange={(event) => setIcon(event.target.value)} className="hidden" />
-                        <Image src={'weightlifting.svg'} alt='Exercise' width={'25'} height={'25'} />
-                    </label>
-                    <label className="cursor-pointer p-3 bg-zinc-400 rounded-xl has-checked:bg-(--color-1) hover:bg-(--color-1)">
-                        <input type="radio" name="icon" id="icon5" value={'ironing-board.svg'} checked={icon === "ironing-board.svg"} onChange={(event) => setIcon(event.target.value)} className="hidden" />
-                        <Image src={'ironing-board.svg'} alt='Cleaning' width={'25'} height={'25'} />
-                    </label>
-                    <label className="cursor-pointer p-3 bg-zinc-400 rounded-xl has-checked:bg-(--color-1) hover:bg-(--color-1)">
-                        <input type="radio" name="icon" id="icon6" value={'coffee-cup.svg'} checked={icon === "coffee-cup.svg"} onChange={(event) => setIcon(event.target.value)} className="hidden" />
-                        <Image src={'coffee-cup.svg'} alt='Relax' width={'25'} height={'25'} />
-                    </label>
+            <Form action={handleSubmit} className="flex flex-col h-full justify-between overflow-auto">
+                <div className="flex flex-col items-start gap-1">
+                    <label htmlFor="title" className="font-medium">Task name</label>
+                    <input type="text" id="title" name="title" placeholder="Task name" value={title} onChange={(event) => setTitle(event.target.value)} className="border rounded-sm p-2 w-full" />
+                    <label htmlFor="description">Description</label>
+                    <textarea name="description" id="description" placeholder="Description" value={description} onChange={(event) => setDescription(event.target.value)} className="border rounded-sm p-2 h-40 w-full"></textarea>
+                    <input type="hidden" name="idTask" value={id} />
+                    <p>Icon</p>
+                    <div className="flex gap-2">
+                        <label className="cursor-pointer p-3 bg-zinc-400 rounded-xl has-checked:bg-(--color-1) hover:bg-(--color-1)">
+                            <input type="radio" name="icon" id="icon1" value={'/joystick.svg'} checked={icon === "/joystick.svg"} onChange={(event) => setIcon(event.target.value)} className="hidden" />
+                            <Image src={'/joystick.svg'} alt='Console' width={'25'} height={'25'} />
+                        </label>
+                        <label className="cursor-pointer p-3 bg-zinc-400 rounded-xl has-checked:bg-(--color-1) hover:bg-(--color-1)">
+                            <input type="radio" name="icon" id="icon2" value={'online-learning.svg'} checked={icon === "online-learning.svg"} onChange={(event) => setIcon(event.target.value)} className="hidden" />
+                            <Image src={'online-learning.svg'} alt='Study' width={'25'} height={'25'} />
+                        </label>
+                        <label className="cursor-pointer p-3 bg-zinc-400 rounded-xl has-checked:bg-(--color-1) hover:bg-(--color-1)">
+                            <input type="radio" name="icon" id="icon3" value={'pingpong.svg'} checked={icon === "pingpong.svg"} onChange={(event) => setIcon(event.target.value)} className="hidden" />
+                            <Image src={'pingpong.svg'} alt='Play' width={'25'} height={'25'} />
+                        </label>
+                        <label className="cursor-pointer p-3 bg-zinc-400 rounded-xl has-checked:bg-(--color-1) hover:bg-(--color-1)">
+                            <input type="radio" name="icon" id="icon4" value={'weightlifting.svg'} checked={icon === "weightlifting.svg"} onChange={(event) => setIcon(event.target.value)} className="hidden" />
+                            <Image src={'weightlifting.svg'} alt='Exercise' width={'25'} height={'25'} />
+                        </label>
+                        <label className="cursor-pointer p-3 bg-zinc-400 rounded-xl has-checked:bg-(--color-1) hover:bg-(--color-1)">
+                            <input type="radio" name="icon" id="icon5" value={'ironing-board.svg'} checked={icon === "ironing-board.svg"} onChange={(event) => setIcon(event.target.value)} className="hidden" />
+                            <Image src={'ironing-board.svg'} alt='Cleaning' width={'25'} height={'25'} />
+                        </label>
+                        <label className="cursor-pointer p-3 bg-zinc-400 rounded-xl has-checked:bg-(--color-1) hover:bg-(--color-1)">
+                            <input type="radio" name="icon" id="icon6" value={'coffee-cup.svg'} checked={icon === "coffee-cup.svg"} onChange={(event) => setIcon(event.target.value)} className="hidden" />
+                            <Image src={'coffee-cup.svg'} alt='Relax' width={'25'} height={'25'} />
+                        </label>
+                    </div>
+                    <>Status</>
+                    <div className="w-full columns-1 md:columns-2 mb-2">
+                        <label className="border-2 border-zinc-400 rounded-2xl p-2 flex items-center gap-2 cursor-pointer hover:border-blue-600 justify-between has-checked:border-blue-600 mb-2">
+                            <input type="radio" name="status" id="status1" className="hidden peer" value={"TODO"} checked={state === "TODO"} onChange={(event) => setState(event.target.value)} />
+                            <Image src={'Time_atack_duotone.svg'} alt='to do' width={20} height={20} className="bg-(--color-1-dark) rounded-2xl p-3 w-fit" />
+                            <p>In Progress</p>
+                            <Image src={'Done_round.svg'} alt='done' width={20} height={20} className="peer-checked:bg-blue-600 peer-checked:visible rounded-full invisible" />
+                        </label>
+                        <label className="border-2 border-zinc-400 rounded-2xl p-2 flex items-center gap-2 cursor-pointer hover:border-blue-600 justify-between has-checked:border-blue-600 mb-2">
+                            <input type="radio" name="status" id="status2" className="hidden peer" value={"WONT_DO"} checked={state === "WONT_DO"} onChange={(event) => setState(event.target.value)} />
+                            <Image src={'close_ring_duotone.svg'} alt='to do' width={20} height={20} className="bg-(--color-3-dark) rounded-2xl p-3 w-fit" />
+                            <p>Won&apos;t do</p>
+                            <Image src={'Done_round.svg'} alt='done' width={20} height={20} className="peer-checked:bg-blue-600 peer-checked:visible rounded-full invisible" />
+                        </label>
+                        <label className="border-2  border-zinc-400 rounded-2xl p-2 flex items-center gap-2 cursor-pointer hover:border-blue-600 justify-between has-checked:border-blue-600 mb-2">
+                            <input type="radio" name="status" id="status3" className="hidden peer" value={"COMPLETED"} checked={state === "COMPLETED"} onChange={(event) => setState(event.target.value)} />
+                            <Image src={'Done_round_duotone.svg'} alt='to do' width={20} height={20} className="bg-(--color-4-dark) rounded-2xl p-3 w-fit" />
+                            <p>Completed</p>
+                            <Image src={'Done_round.svg'} alt='done' width={20} height={20} className="peer-checked:bg-blue-600 peer-checked:visible rounded-full invisible" />
+                        </label>
+                    </div>
                 </div>
-                <>Status</>
-                <div className="w-full columns-1 md:columns-2">
-                    <label className="border-2 border-zinc-400 rounded-2xl p-2 flex items-center gap-2 cursor-pointer hover:border-blue-600 justify-between has-checked:border-blue-600 mb-2">
-                        <input type="radio" name="status" id="status1" className="hidden peer" value={"TODO"} checked={state === "TODO"} onChange={(event) => setState(event.target.value)} />
-                        <Image src={'Time_atack_duotone.svg'} alt='to do' width={20} height={20} className="bg-(--color-1-dark) rounded-2xl p-3 w-fit" />
-                        <p>In Progress</p>
-                        <Image src={'Done_round.svg'} alt='done' width={20} height={20} className="peer-checked:bg-blue-600 peer-checked:visible rounded-full invisible" />
-                    </label>
-                    <label className="border-2 border-zinc-400 rounded-2xl p-2 flex items-center gap-2 cursor-pointer hover:border-blue-600 justify-between has-checked:border-blue-600 mb-2">
-                        <input type="radio" name="status" id="status2" className="hidden peer" value={"WONT_DO"} checked={state === "WONT_DO"} onChange={(event) => setState(event.target.value)}/>
-                        <Image src={'close_ring_duotone.svg'} alt='to do' width={20} height={20} className="bg-(--color-3-dark) rounded-2xl p-3 w-fit" />
-                        <p>Won&apos;t do</p>
-                        <Image src={'Done_round.svg'} alt='done' width={20} height={20} className="peer-checked:bg-blue-600 peer-checked:visible rounded-full invisible" />
-                    </label>
-                    <label className="border-2  border-zinc-400 rounded-2xl p-2 flex items-center gap-2 cursor-pointer hover:border-blue-600 justify-between has-checked:border-blue-600 mb-2">
-                        <input type="radio" name="status" id="status3" className="hidden peer" value={"COMPLETED"} checked={state === "COMPLETED"} onChange={(event) => setState(event.target.value)}/>
-                        <Image src={'Done_round_duotone.svg'} alt='to do' width={20} height={20} className="bg-(--color-4-dark) rounded-2xl p-3 w-fit" />
-                        <p>Completed</p>
-                        <Image src={'Done_round.svg'} alt='done' width={20} height={20} className="peer-checked:bg-blue-600 peer-checked:visible rounded-full invisible" />
-                    </label>
-                </div>
-            </div>
-            <div className="flex relative self-end gap-6">
-                
-                <button type="submit" className="flex items-center gap-2 bg-blue-600 px-6 py-2 rounded-full cursor-pointer text-white">
-                    Save
-                    <Image src={'Done_round.svg'} alt='submit' width={25} height={25} />
-                </button>
+                <div className="flex relative self-end gap-6">
 
-            </div>
-        </Form>
-        <DeleteButton idTask={id} />
+                    <DeleteButton idTask={id} />
+                    
+                    <button type="submit" className="flex items-center gap-2 bg-blue-600 px-6 py-2 rounded-full cursor-pointer text-white">
+                        Save
+                        <Image src={'Done_round.svg'} alt='submit' width={25} height={25} />
+                    </button>
+
+                </div>
+            </Form>
+
         </>
     )
 }
